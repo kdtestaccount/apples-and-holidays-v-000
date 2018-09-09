d8a8ec8dd2cbd02cacd7df10c17380d0939f1436
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-09 17:09:53 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,13 +18,20 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
+  
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  
+  holiday_hash[:winter].each do |attribute, data|
+    if attribute == :christmas || attribute == :new_years
+      data << supply
+    end
+  end
 end
 
 
@@ -32,17 +39,30 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
 
+  holiday_hash[:spring].each do |attribute, data|
+    if attribute == :memorial_day
+      data << supply
+    end
+  end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][:"#{holiday_name}"] = supply_array 
+  holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
-
+  new_data_array = []
+  holiday_hash.each do |season|
+    if season == :winter
+      season.each do |attribute, data|
+         supplies_array << data
+      end
+    supplies_array
+    end   
+  end
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,7 +73,16 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  
+  holiday_hash.each do |season_string, data_item|
+    season_name = season_string.to_s.capitalize
+    puts season_name + ':'
+      data_item.each do |holiday, data|
+        new_holiday_string = holiday[/ _ /] = ''
+        holiday_string = new_holiday_string.to_s.capitalize
+        puts '  ' + holiday_string + ': ' + data.join(', ')
+    end   
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
