99c622729e53ef5c5a8ed87dc3efdaf70796981b
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-09 17:45:09 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,6 @@ def all_winter_holiday_supplies(holiday_hash)
     if season == :winter
       season.each do |attribute, data|
          data {|item| new_data_array << item}
-       end 
       end
     end 
      new_data_array
