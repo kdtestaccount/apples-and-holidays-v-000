ff3381d1f5b617752b8393aa4c7580a9b78fa245
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-09 17:50:07 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,7 +62,7 @@ def all_winter_holiday_supplies(holiday_hash)
     end
     new_data_array
   end
-end
+
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
