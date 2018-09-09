e77a060bf96610a9557368e531fb962996f3927c
kvdoherty@gmail.com <kvdoherty@gmail.com>
2018-09-09 17:31:16 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -93,7 +93,14 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   
   holiday_hash.each do |season_string, data_item|
-    data_item.each do ||
+    data_item.each do |holiday_name, holiday_supplies|
+     holiday_supplies.each do |item|
+       if item == "BBQ"
+         holiday_name
+        end
+      end
+    end
+  end
 end
 
 
